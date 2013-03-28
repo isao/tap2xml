c2fae1b22a6e974489e4d28eeee8abe305b4b18b
Isao Yagi <isao@yahoo-inc.com>
2013-03-27 17:32:09 -0700

    minor

--- xml-out.js
+++ xml-out.js
@@ -1,12 +1,11 @@
-var fs = require('fs'),
+var read = require('fs').readFileSync,
     hb = require('handlebars');
 
 
 function main(data) {
     var tmpl = __dirname + '/template.xml',
-        hbjs = hb.compile(fs.readFileSync(tmpl, 'utf-8'));
+        hbjs = hb.compile(read(tmpl, 'utf-8'));
 
-    //fs.writeFileSync(__dirname + '/tests/results.xml', hbjs(data));
     return hbjs(data);
 }
 
